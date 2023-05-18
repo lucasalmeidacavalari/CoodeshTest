a04a1753b566910963a3758d3c94dc2740ddf69f
Lucas Cavalari <lucas.eduardo1203@hotmail.com>
2023-05-18 12:32:32 -0300

    unit_test_example_for_entities

---
+++ CoodeshTest.Domain.Tests/CreatorUnitTest1.cs
@@ -0,0 +1,24 @@
+using CoodeshTest.Domain.Entities;
+using FluentAssertions;
+
+namespace CoodeshTest.Domain.Tests
+{
+    public class CreatorUnitTest1
+    {
+        [Fact(DisplayName = "Create Creator With Valid State")]
+        public void CreateCreator_WithValidParameters_ResultObjectValidState()
+        {
+            Action action = () => new Creator(1, "Lucas");
+            action.Should()
+                .NotThrow<CoodeshTest.Domain.Validation.DomainExceptionValidation>();
+        }
+
+        [Fact(DisplayName = "Creating Creator With Invalid Name")]
+        public void CreateCreator_InvalidName_DomainExceptionInvalid()
+        {
+            Action action = () => new Creator(1, "Ab");
+            action.Should()
+                .Throw<CoodeshTest.Domain.Validation.DomainExceptionValidation>().WithMessage("Invalid name. minimium 3 charecters");
+        }
+    }
+}
