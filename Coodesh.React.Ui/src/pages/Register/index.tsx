4815b8fa1da21da7661c91fe27a2ccf20e65b7c9
Lucas Cavalari <lucas.eduardo1203@hotmail.com>
2023-05-21 18:26:18 -0300

    creating_registration_page

--- Coodesh.React.Ui/src/pages/Register/index.tsx
+++ Coodesh.React.Ui/src/pages/Register/index.tsx
@@ -1,7 +1,34 @@
+import './register.scss';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 export default function Register() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+  
+    function handleRegister(e: any) {
+      e.preventDefault();
+      if (email !== '' && password !== '') {
+        alert("Boa")
+      } else {
+        alert("Preencha os dados de acesso!");
+      }
+    }
+  
     return (
-        <div>
-            <h1>Pagina Register</h1>
-        </div>
-    )
-}
+      <div className="container-home" onSubmit={handleRegister}>
+        <h1>Coodesh Test</h1>
+        <span>Crie uma nova conta!</span>
+  
+        <form className='form-home'>
+          <input type="email" placeholder='Digite seu email...' name="email" id="email" value={email} onChange={e => setEmail(e.target.value)} />
+          <input type="password" placeholder='*****' name="password" id="password" value={password} onChange={e => setPassword(e.target.value)} />
+          <button type="submit">Registrar</button>
+        </form>
+  
+        <Link className='button-link' to={'/'}>
+          Já possui uma conta? Faça login!
+        </Link>
+      </div>
+    );
+  }
