1543b84264bdd62e1585a6b50f6e6922c2142b97
Lucas Cavalari <lucas.eduardo1203@hotmail.com>
2023-05-21 18:16:32 -0300

    creating_login_screen_and_organizing_front_project

--- Coodesh.React.Ui/src/index.tsx
+++ Coodesh.React.Ui/src/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
-import App from './App';
+import RoutesApp from './routes';
+import { BrowserRouter } from 'react-router-dom';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <RoutesApp />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
